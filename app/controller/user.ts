c7d9f74a7865f38700e0af6aa032feee6f9f2b65
AlienZhaolin <601996845@qq.com>
2020-10-29 16:31:30 +0800

    提交

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -1,10 +1,61 @@
 import { Controller } from 'egg';
 import { post, get } from '../decorator/router';
+import axios from 'axios';
+import { OAUTH_CLIENT_ID, OAUTH_SECRET_KEY, ACCESS_TOKEN_URL } from '../utils/const';
+
 /**
  * @Controller 管理端->用户模块
  */
 export default class User extends Controller {
-
+  /**
+   * @summary github 授权登录
+   * @description 用github登录
+   * @router get /user/get/oauth
+   * @request path
+   * @response 200 baseResponse 创建成功
+   */
+  @get('/user/get/oauth')
+  public async oauthLogin() {
+    const sendClientMes = [ 'login', 'avatar_url', 'id', 'html_url', 'name' ];
+    const { ctx } = this;
+    const { code } = ctx.request.query;
+    const acccessTokenUrl = `${ACCESS_TOKEN_URL}client_id=${OAUTH_CLIENT_ID}&client_secret=${OAUTH_SECRET_KEY}&code=${code}`;
+    const access_token = await axios({
+      method: 'post',
+      url: acccessTokenUrl,
+      headers: {
+        accept: 'application/json',
+      },
+    } as any).then(res => res.data.access_token);
+    const res = await axios.get('https://api.github.com/user', {
+      headers: {
+        accept: 'application/json',
+        Authorization: `token ${access_token}`,
+      },
+    }).then(res => res.data);
+    const cookies = {};
+    sendClientMes.forEach(item => {
+      cookies[item] = res[item];
+    });
+    ctx.cookies.set('githubInfo', JSON.stringify(cookies));
+    ctx.status = 302;
+    ctx.redirect('http://127.0.0.1:5000/home');
+  }
+  /**
+   * @summary oauth登录 - github
+   * @description 用github登录
+   * @router post user/github/oauth
+   * @request path
+   * @response 200 baseResponse 创建成功
+   */
+  @post('/user/github/oauth')
+  public async oauthLoginGithub() {
+    const redirect_uri = 'http://127.0.0.1:7001/user/get/oauth';
+    const githubAuthorizeUrl = 'http://github.com/login/oauth/authorize?';
+    const url = `${githubAuthorizeUrl}client_id=${OAUTH_CLIENT_ID}&redirect_uri=${redirect_uri}`;
+    const { ctx } = this;
+    ctx.helper.redirect({ ctx, data: url, message: '重定向' });
+  }
   /**
    * @summary 获取用户信息
    * @description 登陆获取用户个人信息
--- app/utils/const.ts
+++ app/utils/const.ts
@@ -4,6 +4,7 @@ export const CODE = {
   success: /* 成功返回 */ 0,
   fail: /* 返回失败 */  500,
   expire: /* token过期 */ 401,
+  redirect: 302,
 };
 
 export const DEFAULT_MESSAGE = {
@@ -11,3 +12,14 @@ export const DEFAULT_MESSAGE = {
   fail: '请求失败！',
   expire: 'token过期请从新登陆',
 };
+
+/* oauth : github:Client ID*/
+export const OAUTH_CLIENT_ID = '48769c556137f567caaa'
+;
+
+/* oauth:github: secret */
+export const OAUTH_SECRET_KEY = '14959a44d8f7a6aa1bcb69b4a442966d02bc4ff9'
+;
+
+/* gethub: get AccessToken url  */
+export const ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token?';
